4d674294f9c3ca9d7f7143197c4ce1f7e8b57311
Robinjud <rbnjd7@gmail.com>
2023-09-16 20:01:01 -0500

    boiler plate with form and list components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"; 
+import ShoppingForm from "./Components/ShoppingForm/ShoppingForm";
+import ShoppingList from "./Components/ShoppingList/ShoppingList";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Shopping List</h1>
       </header>
+      <main>
+        <ShoppingForm />
+        <ShoppingList />
+      </main>
     </div>
   );
 }
