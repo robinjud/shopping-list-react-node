d1b599c9339fecc0f5cff3644f39f54160905d32
Robinjud <rbnjd7@gmail.com>
2023-10-11 16:37:31 -0500

    format document with prettier

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,31 +1,43 @@
 import React from "react";
-import connectImg from './Images/icons8-facebook-48.png';
-import connectImg1 from './Images/icons8-x-48.png';
-import connectImg2 from './Images/icons8-instagram-48.png';
-import connectImg3 from './Images/icons8-linkedin-48.png';
-import connectImg4 from './Images/icons8-youtube-48.png';
+import connectImg from "./Images/icons8-facebook-48.png";
+import connectImg1 from "./Images/icons8-x-48.png";
+import connectImg2 from "./Images/icons8-instagram-48.png";
+import connectImg3 from "./Images/icons8-linkedin-48.png";
+import connectImg4 from "./Images/icons8-youtube-48.png";
 
-
-const Footer=() => {
+const Footer = () => {
   return (
     <div className="Footer">
       <h3 className="Footer-title">Follow Us</h3>
-       
+
       <ul className="social-icons-logos">
-        
-        <li className="social-icons" ><img src={connectImg} alt="facebook-logo"/>Facebook</li>
-      
-        <li className="social-icons"><img src={connectImg1} alt="x-logo"/>X</li>
-       
-        <li className="social-icons" > <img src={connectImg2} alt="instragrm-logo"/>Instagram</li>
-        
-        <li className="social-icons"><img src={connectImg3} alt="linkedin logo" />Linkedin</li>
-    
-        <li className="social-icons"><img src={connectImg4} alt="youtube-logo"/>Youtube</li>
+        <li className="social-icons">
+          <img src={connectImg} alt="facebook-logo" />
+          Facebook
+        </li>
+
+        <li className="social-icons">
+          <img src={connectImg1} alt="x-logo" />X
+        </li>
+
+        <li className="social-icons">
+          {" "}
+          <img src={connectImg2} alt="instragrm-logo" />
+          Instagram
+        </li>
+
+        <li className="social-icons">
+          <img src={connectImg3} alt="linkedin logo" />
+          Linkedin
+        </li>
+
+        <li className="social-icons">
+          <img src={connectImg4} alt="youtube-logo" />
+          Youtube
+        </li>
       </ul>
-       </div>
-    
+    </div>
   );
-}
+};
 
 export default Footer;
--- src/Components/ShoppingList/ShoppingList.jsx
+++ src/Components/ShoppingList/ShoppingList.jsx
@@ -36,20 +36,18 @@ function ShoppingItem(props) {
   }
 
   return (
-    
     <li className="list" placeholder="item">
-     <div className="item-list"> {content} </div>
-    
-     <div className="main-button" >
-      <button className="delete-button" onClick={deleteClicked}>
-        Delete
-      </button>
-      <button className="edit-button" onClick={updateClicked}>
-        {isEditable ? "Cancel" : "Edit"}
-      </button>
-     </div>
+      <div className="item-list"> {content} </div>
+
+      <div className="main-button">
+        <button className="delete-button" onClick={deleteClicked}>
+          Delete
+        </button>
+        <button className="edit-button" onClick={updateClicked}>
+          {isEditable ? "Cancel" : "Edit"}
+        </button>
+      </div>
     </li>
-    
   );
 }
 
